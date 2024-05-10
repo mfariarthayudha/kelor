493c87eb768af29759e34b37c092aca858cca010
Jose-Rafaels <jose.rafael1611@gmail.com>
2024-05-10 16:41:30 +0700

    change base url

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3100";
+const baseUrl = "https://kelor.id";
 var res;
 function capitalize(text) {
   return text.replace(/\b\w/g, function (char) {
--- src/controllers/api/account-controller.ts
+++ src/controllers/api/account-controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import knex from "../../utilities/knex";
 export const updateAccount = async (request: Request, response: Response) => {
   const update = await knex("users")
-    // @ts-ignore
     .where("user_id", request.session.userId)
     .update({ name: request.body.name, role: request.body.role })
     .then((res) => {
