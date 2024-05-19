633f765f04675659559fe85d8712e62d5d216c2f
Jose-Rafaels <jose.rafael1611@gmail.com>
2024-05-19 16:48:47 +0700

    change pdf print

--- src/controllers/web/documents-controller.ts
+++ src/controllers/web/documents-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import knex from "../../utilities/knex";
-import htmlToPdf from "../../utilities/htmlToPdf";
+// import htmlToPdf from "../../utilities/htmlToPdf";
 import moment from "moment";
 moment().local();
 moment.locale("id");
@@ -11,20 +11,16 @@ export const getDocumentAll = (request: Request, response: Response) => {
 export const viewSurat = async (request: Request, response: Response) => {
   const document_result_id = request.params.documentResultId;
   try {
-    let surat = await knex("document_results")
+    const surat = await knex("document_results")
       .select("document_result_id", "document_type_id", "content", "signed")
       .where("document_result_id", document_result_id)
       .then((res) => {
         return res[0];
       });
-
-    surat.content = surat.content.replaceAll(
-      "(base_url)",
-      String(process.env.BASE_URL_ONLINE)
-    );
-
-    const pdf = await htmlToPdf(surat["content"]);
-    response.status(200).contentType("application/pdf").send(pdf);
+    let isi = surat["content"];
+    isi = isi.replaceAll("(base_url)", String(process.env.BASE_URL_ONLINE));
+    isi = isi.replace("</body>", "<script> window.print() </script></body>");
+    response.status(200).send(isi);
   } catch (error) {
     console.log(error);
     response.status(500).send({ errorMessage: error });
